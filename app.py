ebed69df12c737a3bbe383efb6e012e53135b0f7
geograFie <f.geo@posteo.de>
2021-03-24 19:11:35 +0100

    everything working, netcdf looks okay, minor adjustments still necessary

--- app.py
+++ app.py
@@ -13,61 +13,37 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 
-from tools.container import Metadata, DataContainer
+from tools.container import DataContainer
 from tools.data import Data
 from tools.xr import XR_Attrs
 
 
 #%% DATA
 data_path = Path('../data/mose/')
-xls_file = 'test.xls'
+xls_file = 'metadata.xls'
 
 data = pd.read_csv(data_path / 'MOSE1_TK3Result_Lindenberg.csv')
+meta = pd.read_excel(data_path / 'metadata.xls',
+                     sheet_name='main', na_values=['NaN','nan','n/a'])
 
-metadata = pd.read_excel(data_path / xls_file,
-                     sheet_name='csv2nc',
-                     na_values=['NaN','nan','n/a'])
+list_local_attrs = ['long_name', 'units', '_FillValue','ancillary_variables',
+              'bounds', 'cell_methods', 'coordinates','comment', 'instrument']
 
 global_attrs = pd.read_excel(data_path / xls_file,
-                             sheet_name='global attributes',
+                             sheet_name='global_attributes',
                              na_values=['NaN','nan','n/a'])
 
 dims_attrs = pd.read_excel(data_path / xls_file,
-                      sheet_name='coordinates',
+                      sheet_name='dimensions',
                       na_values=['NaN','nan','n/a'])
 
-DC = DataContainer(data=data,
-                   metadata=metadata,
-                   global_attrs=global_attrs,
-                   dims_attrs=dims_attrs)
-
+DC = DataContainer(data,meta,global_attrs,dims_attrs,list_local_attrs)
 
 #%% MAIN
 
-m = Metadata(DC)
-target = m.target_df()
-
-D = Data(data,target,
-         time_index='T_mid',
-         time_vars=['T_begin','T_end'])
-
-dst = D.preprocessing()
-
-
-incl_local = ['long_name', 'units', '_FillValue','ancillary_variables',
-              'bounds', 'cell_methods', 'coordinates','comment', 'instrument']
-incl_global = ['title','institution','source','history','references',
-                'comment','campaign','featureType']
-
-local_attrs = C.df
-
-X = XR_Attrs(dst,
-             local_attrs,
-             incl_local,
-             global_attrs,
-             incl_global,
-             coords)
-
+da = Data(DC,time_index='T_mid',time_vars=['T_begin','T_end'])
+dst = da.xr
+X = XR_Attrs(dst,DC)
 dst = X.assign_global_attrs()
 dst = X.assign_local_attrs()
 dst = X.expand_dims()
--- tools/container.py
+++ tools/container.py
@@ -3,18 +3,7 @@
 import pandas as pd
 import numpy as np
 
-# from .tools import Preprocessing
-
-# def strip_spaces(df):
-#     print('removing spaces')
-#     """remove leading and trailing spaces"""
-#     df = df.copy()  # not necessary but to prevent CopyWarning
-#     tmp = df.select_dtypes(['object'])
-#     df.loc[:, tmp.columns] = tmp.apply(lambda x: x.str.strip())
-#     return df
-
 def strip_spaces(df, varnames):
-    print('removing spaces')
     """remove leading and trailing spaces"""
     df = df.copy()  # not necessary but to prevent CopyWarning
     # tmp = self.df.select_dtypes(['object'])
@@ -24,7 +13,7 @@ def strip_spaces(df, varnames):
 
 
 class DataContainer:
-    def __init__(self,data,metadata,global_attrs,dims_attrs):
+    def __init__(self,data,metadata,global_attrs,dims_attrs, list_local_attrs):
         self.data = data
         self.metadata = self.pp(metadata)
 
@@ -35,10 +24,12 @@ class DataContainer:
         self.nc_rename = self.rename()
 
         # df = df[df['standard_name'].isnull() == False]
-        self.global_attrs = global_attrs
-        self.dims_attrs = dims_attrs
-
-        self.dims = list(self.dims_attrs['variable'])
+        self.global_attrs = global_attrs.set_index('attribute')
+        self.list_local_attrs = list_local_attrs
+        self.local_attrs = self.metadata[self.list_local_attrs]
+        self.dims_attrs = strip_spaces(
+            dims_attrs,['standard_name','variable']).set_index('variable')
+        self.dims = list(self.dims_attrs.index)
 
         self.unit_conv = self.unit_conversion()
 
@@ -86,15 +77,15 @@ class DataContainer:
         return unit_conv
 
 
-class Metadata:
-    def __init__(self, DataContainer):
-        self.DC = DataContainer
-        self.src_names = self.DC.src_names
-        self.dst_names = self.DC.dst_names
-        self.metadata = self.DC.metadata
-
-        self.unit_conv = self.unit_conversion()
-        self._FillValue = list(self.metadata['_FillValue'])
+# class Metadata:
+#     def __init__(self, DataContainer):
+#         self.DC = DataContainer
+#         self.src_names = self.DC.src_names
+#         self.dst_names = self.DC.dst_names
+#         self.metadata = self.DC.metadata
+#
+#         self.unit_conv = self.unit_conversion()
+#         self._FillValue = list(self.metadata['_FillValue'])
 
 
     # def unit_conversion(self):
@@ -112,18 +103,18 @@ class Metadata:
     #             for dst, conv in zip(self.dst_names, conv_list)}
     #     return unit_conv
 
-    def target_df(self):
-        self.check_dupl()
-
-        dst = pd.DataFrame(zip(self.src_names,
-                               self.unit_conversion().values()),
-                           index=self.dst_names,
-                           columns=['src_name', 'conv_factor'])
-        dst['unit_conversion'] = dst.index + dst['conv_factor']
-        dst['_FillValue'] = self._FillValue
-
-                for i in self.local_attrs:
-                    dst[i] = self.df.loc[i]
-
-                return self.df[list(self.names())]
-        return dst
+    # def target_df(self):
+    #     self.check_dupl()
+    #
+    #     dst = pd.DataFrame(zip(self.src_names,
+    #                            self.unit_conversion().values()),
+    #                        index=self.dst_names,
+    #                        columns=['src_name', 'conv_factor'])
+    #     dst['unit_conversion'] = dst.index + dst['conv_factor']
+    #     dst['_FillValue'] = self._FillValue
+    #
+    #             for i in self.local_attrs:
+    #                 dst[i] = self.df.loc[i]
+    #
+    #             return self.df[list(self.names())]
+    #     return dst
